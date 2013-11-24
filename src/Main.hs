c299651bdaebd1598faac8a9653aa664eb5f6ac0
Ben Edwards <edwards.benj@gmail.com>
2013-11-24 18:51:42 +0000

    Cleaned up code and added command line options
     * Refactored the shake rules
     * Separated the shake rules for shared src, vs own project src

--- src/Main.hs
+++ src/Main.hs
@@ -1,32 +1,84 @@
 module Main (main) where
 
+import Control.Applicative
+import Control.Monad
 import Data.Char (isSpace)
 import Data.List
 import Data.Monoid ((<>))
 import Development.Shake
-import Control.Applicative
+import Development.Shake.FilePath
+import Options.Applicative
 import System.Process
 
 import ShakeRules
 
-main :: IO ()
-main = do
-    db <- getPackageDbPath
-    packageList <-  maybe (return []) getPackageList db
-    putStr $ intercalate "," packageList
-    let base = "/home/ben/src/haskell"
-    shake shakeOptions $ do
-        fetchArchiveRule base
-        buildTagRule base
-        buildThisTagRule
-        mapM_ (buildWants base) packageList
+data Snuffle = Snuffle
+    { buildDeps :: Bool   -- ^ should build tags for dependencies
+    , buildOwn  :: Bool   -- ^ should build tags for own source
+    , pathDeps  :: String -- ^ path to folder for dependent source
+    }
 
+snuffle :: Parser Snuffle
+snuffle = Snuffle
+    <$> switch
+        ( long "build-deps"
+       <> short 'd'
+       <> value False
+       <> help "should snuffle build a tags file for each cabal dependency"
+        )
+    <*> switch
+        ( long "build-own"
+       <> short 'o'
+       <> value True
+       <> help "should snuffle build a tags file for the project source"
+        )
+    <*> strOption
+        ( long "src-path"
+       <> short 'p'
+       <> value "~/.hs-snuffle/share/src"
+       <> metavar "PATH"
+       <> help "where should snuffle store fetched source files"
+        )
+
+opts = info (helper <*> snuffle) desc
+  where
+    desc = fullDesc <> pDesc <> pHeader
+    pDesc = progDesc $ "Fetch sources and generate tags for cabal sandboxed"
+                        ++ " dependencies. Run in the same directory as any"
+                        ++ " sandboxed cabal project to generate tags for "
+                        ++ "your project source as well as that of any "
+                        ++ "dependent packages"
+    pHeader = header "Snuffle - autogen vim tags for cabal projects!"
+
+-- | unpack snuffle
+withSnuffle :: (Bool -> Bool -> FilePath -> IO ()) -- ^ run snuffle options
+            -> Snuffle                             -- ^ configured options
+            -> IO ()
+withSnuffle f (Snuffle deps own path) = f deps own path
+
+main :: IO ()
+main = execParser opts >>= withSnuffle go
+  where
+    go deps own path = do
+        when own $ shake shakeOptions buildThisTagRule
+        when deps $ do
+            db <- getPackageDbPath
+            packageList <- maybe (return []) getPackageList db
+            putStr $ intercalate "," packageList
+            let shakeDeps = shakeOptions { shakeFiles = path </> ".shake" }
+            shake shakeDeps $ do
+                fetchArchiveRule path
+                buildTagRule path
+                buildWants path packageList
+
+-- | get the path to the sandbox package db
 getPackageDbPath :: IO (Maybe String)
 getPackageDbPath = getPath <$> io
   where 
     io = readProcess "grep" ["package-db", "cabal.sandbox.config"] ""
     getPath = stripPrefix "package-db: "
 
+-- | get the list of install packages
 getPackageList :: String -> IO [String]
 getPackageList x = toList <$> io
   where
@@ -34,11 +86,10 @@ getPackageList x = toList <$> io
     io = readProcess "ghc-pkg" ["list","--package-db", clean] ""
     toList = init . tail . map stripStart . lines
 
+-- | leading and trailing whitespace from a string
 strip :: String -> String
 strip = stripStart . takeWhile (not . isSpace)
 
+-- | string leading whitespace from a string
 stripStart :: String -> String
 stripStart = dropWhile isSpace
-   
-
-
--- src/ShakeRules.hs
+++ src/ShakeRules.hs
@@ -1,45 +1,80 @@
-{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleContexts, TypeOperators #-}
 
 module ShakeRules
   where
 
+import Control.Applicative ((<$>))
 import Control.Monad (void)
 import Development.Shake
 import Development.Shake.FilePath
-import System.Process
 
-genTagShell :: String -> String
-genTagShell path = "export LC_ALL=C; cd " ++ path ++ "; find -type f | egrep \\.hs$\\|\\.lhs$ | xargs -Ii hothasktags i | sort > tags 2>/dev/null"
+-- | Shell command for generating an unsorted tags file. The sorting is
+--   split from the tags generation to avoid problems with hGetContents and
+--   invalid byte sequences.
+genTagShell :: String
+genTagShell = "find -type f | egrep \\.hs$\\|\\.lhs$ | xargs -Ii hothasktags i > tags.unsorted"
 
-genTagFile :: String -> Action ()
-genTagFile path = void $ liftIO (system $ genTagShell path)
+-- | sorts the tags file. Must be run with "LC_ALL=C" set in the env.
+sortTagShell :: String
+sortTagShell = "sort tags.unsorted > tags"
 
-buildWants :: String -> String -> Rules ()
-buildWants base x = want [base ++ "/" ++ x ++ "/tags"]
+-- | generate a rule to say that we want a set of tag files.
+buildWants :: String   -- ^ base directory for our source files
+           -> [String] -- ^ list of packages with version numbers
+           -> Rules ()
+buildWants base = want . map f
+  where
+    f x = base </> x </> "tags"
 
-buildTagRule :: String -> Rules ()
+-- | this rule matches all tag files for a versioned pacakge within our
+--   source directory. Given that the source dir exists, make a tags file
+buildTagRule :: String   -- ^ base directory for source files
+             -> Rules ()
 buildTagRule base = do
     (base ++ "/*/tags") *> \out -> do
         let packageDir = takeDirectory out
         need [packageDir]
         genTagFile packageDir
 
+-- | Create a source directory by fetching it from hackage and unpacking
+--   it.
 fetchArchiveRule :: String -> Rules ()
 fetchArchiveRule base = do
     (base ++ "/*") *> \out -> do
-        let archive = out `addExtension` "tar.gz"
+        let archive = takeFileName out `addExtension` "tar.gz"
             packageUrl = "https://hackage.haskell.org/package/" 
                             ++ archive
-            wd = takeDirectory out
-        unit $ cmd (Cwd wd) "wget" [packageUrl]
-        unit $ cmd (Cwd base) "tar -xzf" [archive]
-        unit $ cmd (Cwd base) "rm" [archive]
+            wd = out
+            g cmd' file = u $ cmd (Cwd base) cmd' [file]
+        g "wget" packageUrl
+        g "tar -zxf" archive
+        g "rm" archive
 
+-- | In the case of our own source files (non-remote) we want to regen the
+--   tag file as soon as any of the sources files change
+getSourceFiles :: Action [String]
+getSourceFiles = (lines . fromStdout) <$> cmd Shell shellCmd
+  where
+    shellCmd = "find -type f | egrep \\.hs$\\|\\.lhs$"
+
+-- | build a tags file for the current directory if it doesn't already
+--   exist
 buildThisTagRule :: Rules ()
 buildThisTagRule = do
     want ["tags"]
-    "tags" *> \out -> do
+    "tags" *> \_ -> do
+        need =<< getSourceFiles
         genTagFile "."
 
-unit :: Action () -> Action ()
-unit x = x
+-- | generate a tag file for a given directory 
+genTagFile :: String -> Action ()
+genTagFile wd = do
+    let cwd = Cwd wd
+        env = Env [("LC_ALL", "C")]
+    u $ cmd Shell cwd genTagShell
+    u $ cmd Shell cwd env sortTagShell
+    cmd cwd "rm" ["tags.unsorted"]
+
+-- | force a return type for cmd.
+u :: Action () -> Action ()
+u = id
